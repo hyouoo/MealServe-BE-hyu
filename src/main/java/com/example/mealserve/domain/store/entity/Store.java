94d90c571f7b8c67c9fa31666f11eb42c70bf969
jammy102 <dnjscksv@naver.com>
2023-11-20 11:12:25 +0900

    Feat : Store Entity 구현

---
+++ src/main/java/com/example/mealserve/domain/store/entity/Store.java
@@ -0,0 +1,48 @@
+package com.example.mealserve.domain.store.entity;
+
+import com.example.mealserve.domain.menu.entity.Menu;
+import com.example.mealserve.domain.store.dto.StoreRequestDto;
+import com.example.mealserve.domain.store.dto.StoreResponseDto;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 기본 생성자의 생성을 방지, 지정한 생성자를 사용하도록 강제!
+// PROTECTED: 상속은 가능
+@Getter
+@Entity
+@Table(name = "store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String tel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    @Builder
+    private Store(String name, String address, String tel) {
+        this.name = name;
+        this.address = address;
+        this.tel = tel;
+    }
+
+    public void update(StoreRequestDto storeRequestDto) {
+        this.name = storeRequestDto.getName();
+        this.address = storeRequestDto.getAddress();
+        this.tel = storeRequestDto.getTel();
+    }
+}
