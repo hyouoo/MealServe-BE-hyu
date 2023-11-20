917de86d421dc7f65389fe8ec538386da22a5108
jammy102 <dnjscksv@naver.com>
2023-11-20 14:09:03 +0900

    Feat : Store CRUD 구현

---
+++ src/main/java/com/example/mealserve/domain/store/StoreRepository.java
@@ -0,0 +1,10 @@
+package com.example.mealserve.domain.store;
+
+import com.example.mealserve.domain.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
