651183148efc2999ee170633e184bdc7b1848eb4
min_moon <minhee610@gmail.com>
2023-11-20 11:57:04 +0900

    Update: Dto 약간 수정

--- src/main/java/com/example/mealserve/domain/customer/dto/AccountLoginResponseDto.java
+++ src/main/java/com/example/mealserve/domain/customer/dto/AccountLoginResponseDto.java
@@ -1,9 +1,11 @@
 package com.example.mealserve.domain.customer.dto;
 
 import com.example.mealserve.domain.customer.entity.RoleTypeEnum;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class AccountLoginResponseDto {
 
     private RoleTypeEnum role;
