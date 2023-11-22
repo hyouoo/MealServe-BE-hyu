25fc7556d5942a8027b54cb619a953112dc3e943
Sanghyu Lee <hyouoo@gmail.com>
2023-11-22 17:38:47 +0900

    Fix: Order Domain 에러 수정

--- src/main/java/com/example/mealserve/domain/order/OrderService.java
+++ src/main/java/com/example/mealserve/domain/order/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.mealserve.domain.order;
 
+import com.example.mealserve.domain.account.AccountRepository;
 import com.example.mealserve.domain.account.entity.Account;
 import com.example.mealserve.domain.menu.MenuRepository;
 import com.example.mealserve.domain.menu.entity.Menu;
@@ -14,15 +15,18 @@ import com.example.mealserve.domain.store.entity.Store;
 import com.example.mealserve.global.exception.CustomException;
 import com.example.mealserve.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j(topic = "OrderService")
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private final AccountRepository accountRepository;
 
     private final OrderRepository orderRepository;
     private final MenuRepository menuRepository;
@@ -36,6 +40,7 @@ public class OrderService {
 
         for (OrderRequestDto requestDto : requestDtoList) {
             Menu menu = getMenu(requestDto);
+
             Order newOrder = Order.of(customer, menu, requestDto.getQuantity(), DeliverStatus.PREPARE);
             orderRepository.save(newOrder);
 
@@ -43,7 +48,7 @@ public class OrderService {
             totalPrice += menu.getPrice() * newOrder.getQuantity();
         }
 
-        checkEnoughPoint(customer, totalPrice);
+        checkEnoughPoint(customer, totalPrice).payPoint(totalPrice);
 
         return OrderResponseDto.of(orderDtoList, totalPrice);
     }
@@ -51,37 +56,47 @@ public class OrderService {
 
     @Transactional(readOnly = true)
     public List<OrderListResponseDto> getOrders(Account owner) {
-        Store store = findStore(owner.getStore().getId());
+        Store store = owner.getStore();
+        log.info("fetch join start");
         List<Order> orders = orderRepository.findAllByStoreId(store.getId());
+        log.info("fetch join end");
         List<OrderDto> orderDtoList = new ArrayList<>();
         List<OrderListResponseDto> orderListResponseDtos = new ArrayList<>();
 
-        int i = 0, totalPrice = 0;
-        while (i < orders.size()) {
-            for (int j = 0; j < orders.size(); j++) {
-                Account user = orders.get(i).getAccount();
-                if (user == orders.get(i + j).getAccount()) {
-                    orderDtoList.add(OrderDto.fromOwner(orders.get(i + j)));
-                    totalPrice += orders.get(i + j).getMenu().getPrice()
-                            * orders.get(i + j).getQuantity();
-                } else {
-                    orderListResponseDtos.add(
-                            OrderListResponseDto.of(user, orderDtoList, totalPrice));
-                    i += j;
-                    break;
-                }
+        int i = 0, j = 0, totalPrice = 0;
+        log.info("while start");
+        while (true) {
+            Account user = orders.get(i).getAccount();
+            if (user == orders.get(i + j).getAccount()) {
+                orderDtoList.add(OrderDto.fromOwner(orders.get(i + j)));
+                totalPrice += orders.get(i + j).getMenu().getPrice() * orders.get(i + j).getQuantity();
+                j++;
+            } else {
+                orderListResponseDtos.add(OrderListResponseDto.of(user, orderDtoList, totalPrice));
+                i += j;
+                j = 0;
+                totalPrice = 0;
+                orderDtoList.clear();
+            }
+            if (i + j == orders.size()) {
+                orderListResponseDtos.add(OrderListResponseDto.of(user, orderDtoList, totalPrice));
+                break;
             }
         }
+        log.info("while end");
         return orderListResponseDtos;
     }
 
     @Transactional
     public void completeOrders(Account owner, Long accountId) {
         List<Order> orders = orderRepository.findAllByAccountId(accountId);
+        int totalPrice = 0;
         for (Order order : orders) {
-            owner.earnPoint(order.getMenu().getPrice() * order.getQuantity());
-            order.complete();
+            checkOrderStatus(order).complete();
+            totalPrice += order.getMenu().getPrice() * order.getQuantity();
         }
+        owner.earnPoint(totalPrice);
+        accountRepository.save(owner);
     }
 
     private Store findStore(Long id) {
@@ -91,11 +106,18 @@ public class OrderService {
 
     private Menu getMenu(OrderRequestDto requestDto) {
         return menuRepository.findById(requestDto.getMenuId())
-            .orElseThrow(() -> new CustomException(ErrorCode.MENU_NOT_FOUND));
+                .orElseThrow(() -> new CustomException(ErrorCode.MENU_NOT_FOUND));
     }
 
-    private static void checkEnoughPoint(Account customer, int totalPrice) {
+    private Account checkEnoughPoint(Account customer, int totalPrice) {
         if (customer.getPoint() < totalPrice)
             throw new CustomException(ErrorCode.INSUFFICIENT_POINT);
+        return customer;
+    }
+
+    private Order checkOrderStatus(Order order) {
+        if (order.getStatus().equals(DeliverStatus.COMPLETE))
+            throw new CustomException(ErrorCode.ORDER_ALREADY_COMPLETED);
+        return order;
     }
 }
