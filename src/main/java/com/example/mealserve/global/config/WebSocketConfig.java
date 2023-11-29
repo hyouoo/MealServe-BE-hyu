838c01cafcc1a76ddc61f7008fcfb6848d2a264b
Sanghyu Lee <hyouoo@gmail.com>
2023-11-29 16:01:03 +0900

    Feat: WebSocket Config/Handler 구현

---
+++ src/main/java/com/example/mealserve/global/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.example.mealserve.global.config;
+
+import com.example.mealserve.global.websocket.AlarmHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final AlarmHandler alarmHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(alarmHandler, "/alarm").setAllowedOrigins("*");
+    }
+}
